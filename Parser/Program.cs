4aaaecff1e8c6549815e100558f6a830bd3b4f83
Administrator <Administrator@EURU2610>
2022-10-21 18:55:39 +0300

    Добавьте файлы проекта.

---
+++ Parser/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Parser
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите путь к файлу: ");
+                string path = Console.ReadLine();
+                if (IsValidFile(path))
+                {
+                    Analysis analysis = new Analysis(path);
+                    analysis.Analyze();
+
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.WriteLine("Введите корректный путь к файлу.");
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                }
+            }
+        }
+
+
+        private static bool IsValidFile(string path) => string.IsNullOrWhiteSpace(path) is false &&
+            path.EndsWith(".txt") && File.Exists(path);
+    }
+}
